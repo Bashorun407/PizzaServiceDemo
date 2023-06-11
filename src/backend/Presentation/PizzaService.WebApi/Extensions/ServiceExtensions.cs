5348b33d43caaf19be0f113fda120e3f91202459
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-06-11 18:32:53 +0100

    Committing changes in the Core directory which is LoggerService layer that was added to Core directory; to local repository

---
+++ src/backend/Presentation/PizzaService.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace PizzaService.WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => { });
+        }
+    }
+}
