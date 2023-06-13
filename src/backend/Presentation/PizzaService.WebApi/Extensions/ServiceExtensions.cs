54ae0728487582a74ef53bc4181a9daf4a1346c8
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-06-13 09:50:10 +0100

    Committing Service extension file and others to local repository

--- src/backend/Presentation/PizzaService.WebApi/Extensions/ServiceExtensions.cs
+++ src/backend/Presentation/PizzaService.WebApi/Extensions/ServiceExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace PizzaService.WebApi.Extensions
+﻿using LoggerService;
+using PizzaService.Application.Common;
+using PizzaService.Persistence.Common;
+
+namespace PizzaService.WebApi.Extensions
 {
     public static class ServiceExtensions
     {
@@ -16,6 +20,16 @@
         public static void ConfigureIISIntegration(this IServiceCollection services)
         {
             services.Configure<IISOptions>(options => { });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
         }
     }
 }
